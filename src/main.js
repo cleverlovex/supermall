6046c9f4a6b4e067677b78660082abde650a9e79
Ljw <871460573@qq.com>
2020-06-14 02:16:18 +0800

    '移动端设配以及图片懒加载'

--- src/main.js
+++ src/main.js
@@ -3,9 +3,23 @@ import App from './App.vue'
 import router from "./router";
 import store from './store'
 
+//安装toast插件
 import toast from 'components/common/toast'
 
+import FastClick from'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+
+//添加事件总线对象
 Vue.config.productionTip = false
+
+//解决移动端300ms延迟
+FastClick.attach(document.body)
+
+//使用图片懒加载
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 Vue.prototype.$bus = new Vue()
 
